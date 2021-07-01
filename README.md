0440a2ac5661807406a967c9b08aff461456181d
hkbcode <hkbcode@gmail.com>
2021-07-01 16:47:06 +0530

    setup done with tailwindcss :fire:

---
+++ README.md
@@ -0,0 +1,4 @@
+## Reactjs with TailwindCSS
+
+
+Reactjs starter code with Tailwindcss to build some awesome apps
---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div class="flex justify-center p-3">
+        <h1>Build Apps with tailwindcss</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
